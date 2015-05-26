136d01a280596b196fd31ab7a5ec25fea25685e9
Wolverine <Wolverine@Wolverine-DELL>
2015-05-26 10:05:01 -0300

    Criação dos model e connection

---
+++ src/main/java/br/com/financemate/model/Atividades.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.financemate.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Wolverine
+ */
+@Entity
+@Table(name = "atividades")
+public class Atividades implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idatividades")
+    private Integer idatividades;
+    @Size(max = 100)
+    @Column(name = "nome")
+    private String nome;
+    @Lob
+    @Size(max = 16777215)
+    @Column(name = "comentario")
+    private String comentario;
+    @Column(name = "prazo")
+    @Temporal(TemporalType.DATE)
+    private Date prazo;
+    @Size(max = 30)
+    @Column(name = "prioridade")
+    private String prioridade;
+    @Size(max = 3)
+    @Column(name = "concluidade")
+    private String concluidade;
+    @Column(name = "idrotina")
+    private Integer idrotina;
+    @JoinColumn(name = "subdepartamento_idsubdepartamento", referencedColumnName = "idsubdepartamento")
+    @ManyToOne(optional = false)
+    private Subdepartamento subdepartamento;
+    @JoinColumn(name = "cliente_idcliente", referencedColumnName = "idcliente")
+    @ManyToOne(optional = false)
+    private Cliente cliente;
+    @JoinColumn(name = "usuario_idusuario", referencedColumnName = "idusuario")
+    @ManyToOne(optional = false)
+    private Usuario usuario;
+
+    public Atividades() {
+    }
+
+    public Atividades(Integer idatividades) {
+        this.idatividades = idatividades;
+    }
+
+    public Integer getIdatividades() {
+        return idatividades;
+    }
+
+    public void setIdatividades(Integer idatividades) {
+        this.idatividades = idatividades;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(Date prazo) {
+        this.prazo = prazo;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getConcluidade() {
+        return concluidade;
+    }
+
+    public void setConcluidade(String concluidade) {
+        this.concluidade = concluidade;
+    }
+
+    public Integer getIdrotina() {
+        return idrotina;
+    }
+
+    public void setIdrotina(Integer idrotina) {
+        this.idrotina = idrotina;
+    }
+
+    public Subdepartamento getSubdepartamento() {
+        return subdepartamento;
+    }
+
+    public void setSubdepartamento(Subdepartamento subdepartamento) {
+        this.subdepartamento = subdepartamento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idatividades != null ? idatividades.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Atividades)) {
+            return false;
+        }
+        Atividades other = (Atividades) object;
+        if ((this.idatividades == null && other.idatividades != null) || (this.idatividades != null && !this.idatividades.equals(other.idatividades))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.financemate.model.Atividades[ idatividades=" + idatividades + " ]";
+    }
+    
+}
---
+++ src/main/java/br/com/financemate/model/Perfil.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.financemate.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Wolverine
+ */
+@Entity
+@Table(name = "perfil")
+public class Perfil implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idperfil")
+    private Integer idperfil;
+    @Size(max = 15)
+    @Column(name = "tipoacesso")
+    private String tipoacesso;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "perfil")
+    private List<Usuario> usuarioList;
+
+    public Perfil() {
+    }
+
+    public Perfil(Integer idperfil) {
+        this.idperfil = idperfil;
+    }
+
+    public Integer getIdperfil() {
+        return idperfil;
+    }
+
+    public void setIdperfil(Integer idperfil) {
+        this.idperfil = idperfil;
+    }
+
+    public String getTipoacesso() {
+        return tipoacesso;
+    }
+
+    public void setTipoacesso(String tipoacesso) {
+        this.tipoacesso = tipoacesso;
+    }
+
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idperfil != null ? idperfil.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Perfil)) {
+            return false;
+        }
+        Perfil other = (Perfil) object;
+        if ((this.idperfil == null && other.idperfil != null) || (this.idperfil != null && !this.idperfil.equals(other.idperfil))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.financemate.model.Perfil[ idperfil=" + idperfil + " ]";
+    }
+    
+}
